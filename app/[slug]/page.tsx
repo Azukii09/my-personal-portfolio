1a8d3bc4377ae09cae3901712bb5c48bef5d5324
Azukii09 <azuknisianta@gmail.com>
2024-07-01 11:31:59 +0700

    #1 - adding navbar item and dynamic navbar
    Signed-off-by: Azukii09 <azuknisianta@gmail.com>

---
+++ app/[slug]/page.tsx
@@ -0,0 +1,9 @@
+export default function SinglePage(){
+    return (
+        <div className="fixed px-6 sm:px-8 md:px-16 lg:px-20 top-20 w-full h-full">
+            <div className={"top-20 w-full h-full p-6"}>
+                <div>ini single page</div>
+            </div>
+        </div>
+    )
+}
---
+++ data/navbar.ts
@@ -0,0 +1,26 @@
+const navbar = [
+    {
+        id:1,
+        name:"Hero",
+        link:"/hero"
+    },
+    {
+        id:2,
+        name:"About",
+        link:"/about"
+    },
+    {
+        id:3,
+        name:"Projects",
+        link:"/projects"
+    },
+    {
+        id:4,
+        name:"Contact Us",
+        link:"/contact"
+    },
+]
+
+export const getNavbar = ()=>{
+    return navbar;
+}
