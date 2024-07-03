b446d550b69afa4a83996b626297abf3f39271f5
Azukii09 <azuknisianta@gmail.com>
2024-07-03 22:26:48 +0700

    #3 - adding social media link
    Signed-off-by: Azukii09 <azuknisianta@gmail.com>

--- components/feature/footer.tsx
+++ components/feature/footer.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Footer() {
     return(
@@ -6,10 +7,18 @@ export default function Footer() {
             <div className={"h-20 bg-white flex items-center justify-between"}>
                 <div className={"text-lg text-primary"}>Copyright <span className={"text-xl"}>© 2024</span></div>
                 <div className={"flex gap-4"}>
-                    <Image src={"/assets/img/socmed/wa.png"} alt={"wa"} height={25} width={25}/>
-                    <Image src={"/assets/img/socmed/github.png"} alt={"wa"} height={25} width={25}/>
-                    <Image src={"/assets/img/socmed/instagram.png"} alt={"wa"} height={25} width={25}/>
-                    <Image src={"/assets/img/socmed/linkedin.png"} alt={"wa"} height={25} width={25}/>
+                    <Link href={"https://api.whatsapp.com/send?phone=6281133390006"}>
+                        <Image src={"/assets/img/socmed/wa.png"} alt={"wa"} height={25} width={25}/>
+                    </Link>
+                    <Link href={"https://github.com/Azukii09"}>
+                        <Image src={"/assets/img/socmed/github.png"} alt={"github"} height={25} width={25}/>
+                    </Link>
+                    <Link href={"https://www.instagram.com/alvillage/"}>
+                        <Image src={"/assets/img/socmed/instagram.png"} alt={"instagram"} height={25} width={25}/>
+                    </Link>
+                    <Link href={"https://www.linkedin.com/in/alvillage-zuknisianta-ab5b82137/"}>
+                        <Image src={"/assets/img/socmed/linkedin.png"} alt={"linkedin"} height={25} width={25}/>
+                    </Link>
                 </div>
             </div>
         </footer>
