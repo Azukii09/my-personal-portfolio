b5327ce43c93d58baa74563c5f30d82ea170fe30
Azukii09 <azuknisianta@gmail.com>
2024-07-06 08:57:42 +0700

    #2 - refactoring about pages
    Signed-off-by: Azukii09 <azuknisianta@gmail.com>

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,12 +1,15 @@
-import Image from "next/image";
 import {getServicesAbout} from "@/data/serviceAbout";
 import {getInterstAbout} from "@/data/interestAbout";
-import RadialProgressBar from "@/components/component/radialProgressBar";
 import {getLanguageAbout} from "@/data/languageSkill";
-import ProgressBar from "@/components/component/progressBar";
 import {getSkillAbout} from "@/data/skillData";
-import ExperienceSections from "@/components/feature/experience";
+import ExperienceSections from "@/components/feature/about/experience";
 import {getExperienceAbout} from "@/data/experienceData";
+import BigTitleAbout from "@/components/feature/about/bigTitleAbout";
+import ProfileAbout from "@/components/feature/about/profileAbout";
+import ServiceAbout from "@/components/feature/about/serviceAbout";
+import InterestAbout from "@/components/feature/about/interestAbout";
+import SkillAbout from "@/components/feature/about/skillAbout";
+import LanguageAbout from "@/components/feature/about/languageAbout";
 
 export const metadata = {
     title: "About",
@@ -30,121 +33,37 @@ export default function AboutPage(){
     return (
         <div className="w-full">
             {/*about sections*/}
-            <div className={"flex gap-8 items-center justify-between"}>
-                <h1 className={"uppercase font-bold text-lg tracking-[0.2em]"}>about me</h1>
-                <div className={"bg-ternary h-[2px] w-2/3"}></div>
-            </div>
+            <BigTitleAbout title={"about me"}/>
 
             {/*profile data*/}
-            <div className={"my-12 flex flex-col gap-8 lg:flex-row lg:gap-0 justify-between"}>
-                <div className={"w-full lg:w-5/12 flex flex-col gap-4"}>
-                    <div className={"text-justify indent-5"}>Hi, my name is Alvillage Zuksnisianta! As a freelance and
-                        self-employed Full Stack Web Developer since 2016, I design and develop web applications using a
-                        variety of technologies including Golang, PostgreSQL, MySQL, Docker, Git, React.js, Vue.js,
-                        Laravel, Next.js, Bootstrap, and Tailwind CSS.
-                    </div>
-                    <div className={"text-justify indent-5"}>My responsibilities include creating and managing
-                        databases, integrating APIs, ensuring application performance and security, debugging and
-                        resolving issues, collaborating with cross-functional teams, and maintaining documentation for
-                        ongoing development and future updates. I am also experienced in installing websites on both
-                        shared hosting and VPS platforms.
-                    </div>
-                </div>
-                <div className={"w-full lg:w-5/12 flex flex-col gap-4"}>
-                    <div className={"flex"}>
-                        <div className={"w-2/6 font-bold text-lg capitalizex"}>Name</div>
-                        <div className={"italic text-lg"}>: Alvillage Zuknisianta</div>
-                    </div>
-                    <div className={"flex"}>
-                        <div className={"w-2/6 font-bold text-lg capitalizex"}>Address</div>
-                        <div className={"italic text-lg"}>: Pelang Village, RT 001 RW 002,<br/> Kembangbahu
-                            District, <br/>Lamongan
-                            Regency, <br/>East Java Province,Indonesia
-                        </div>
-                    </div>
-                    <div className={"flex"}>
-                        <div className={"w-2/6 font-bold text-lg capitalizex"}>Phone</div>
-                        <div className={"italic text-lg"}>: (+62) 811 3339 0006</div>
-                    </div>
-                    <div className={"flex"}>
-                        <div className={"w-2/6 font-bold text-lg capitalizex"}>Mail</div>
-                        <div className={"italic text-lg"}>: azuknisinata@gmail.com</div>
-                    </div>
-                </div>
-            </div>
+            <ProfileAbout/>
 
             {/*services and interest*/}
             <div className={"my-12 flex flex-col gap-8 lg:flex-row lg:gap-0 justify-between"}>
+                {/*services section*/}
                 <div className={" w-full lg:w-5/12 flex flex-col gap-4"}>
-                    <div className={"flex gap-8 items-center justify-between"}>
-                        <h1 className={"uppercase font-bold text-lg tracking-[0.2em]"}>services</h1>
-                        <div className={"bg-ternary h-[2px] w-3/4"}></div>
-                    </div>
-                    <div className={"mt-6"}>
-                        <ul className={"flex gap-4 flex-col"}>
-                            {dataService.map((service) => (
-                                <li className={"flex gap-2 items-center"} key={service.id}>
-                                    <div className={"relative w-3 h-2"}>
-                                        <Image src={"/assets/img/play-new.png"} alt={"play"} fill/>
-                                    </div>
-                                    {service.name}
-                                </li>
-                            ))}
-                        </ul>
-                    </div>
+                    <ServiceAbout data={dataService}/>
                 </div>
+                {/*interest section*/}
                 <div className={"w-full lg:w-5/12 flex flex-col gap-4"}>
-                    <div className={"flex gap-8 items-center justify-between"}>
-                        <h1 className={"uppercase font-bold text-lg tracking-[0.2em]"}>interest</h1>
-                        <div className={"bg-ternary h-[2px] w-3/4"}></div>
-                    </div>
-                    <div className={"mt-6"}>
-                        <ul className={"flex gap-4 flex-col"}>
-                            {dataInterest.map((interest) => (
-                                <li className={"flex gap-2 items-center"} key={interest.id}>
-                                    <div className={"relative w-3 h-2"}>
-                                        <Image src={"/assets/img/play-new.png"} alt={"play"} fill/>
-                                    </div>
-                                    {interest.name}
-                                </li>
-                            ))}
-                        </ul>
-                    </div>
+                    <InterestAbout data={dataInterest}/>
                 </div>
             </div>
 
             {/*skills and language*/}
             <div className={"my-12 flex flex-col gap-8 lg:flex-row lg:gap-0 justify-between"}>
+                {/*skill sections*/}
                 <div className={" w-full lg:w-5/12 flex flex-col gap-4"}>
-                    <div className={"flex gap-8 items-center justify-between"}>
-                        <h1 className={"uppercase font-bold text-lg tracking-[0.2em]"}>skill</h1>
-                        <div className={"bg-ternary h-[2px] w-3/4"}></div>
-                    </div>
-                    <div className={"mt-6 flex flex-col gap-4"}>
-                        {dataSkill.map((skill) => (
-                            <ProgressBar percentage={skill.percentage} label={skill.label} key={skill.id}/>
-                        ))}
-                    </div>
+                    <SkillAbout data={dataSkill}/>
                 </div>
+                {/*language section*/}
                 <div className={"w-full lg:w-5/12 flex flex-col gap-4"}>
-                    <div className={"flex gap-8 items-center justify-between"}>
-                        <h1 className={"uppercase font-bold text-lg tracking-[0.2em]"}>language</h1>
-                        <div className={"bg-ternary h-[2px] w-3/4"}></div>
-                    </div>
-                    <div className={"mt-6 flex gap-2 flex-wrap"}>
-                        {dataLanguage.map((language) => (
-                            <RadialProgressBar percentage={language.percentage} language={language.language}
-                                               key={language.id}/>
-                        ))}
-                    </div>
+                    <LanguageAbout data={dataLanguage}/>
                 </div>
             </div>
 
             {/*experience*/}
-            <div className={"flex gap-8 items-center justify-between"}>
-                <h1 className={"uppercase font-bold text-lg tracking-[0.2em]"}>experience</h1>
-                <div className={"bg-ternary h-[2px] w-2/3"}></div>
-            </div>
+            <BigTitleAbout title={"experience"}/>
             <div className={"my-12"}>
                 <ExperienceSections list={getExperienceAbout()}/>
             </div>
--- app/page.tsx
+++ app/page.tsx
@@ -7,8 +7,8 @@ export default function HomePage() {
       router.push("/about");
   }
   return (
-      <div className={"flex flex-col justify-center gap-2"}>
-        <h1 className={"font-bold text-primary text-5xl mt-40"}>Alvillage Zuknisianta</h1>
+      <div className={"flex flex-col justify-center gap-2 h-3/4"}>
+        <h1 className={"font-bold text-primary text-5xl"}>Alvillage Zuknisianta</h1>
         <span className={"text-primary text-lg font-bold"}>Web Developers!</span>
         <span className={"h-[4px] w-36 bg-primary"}></span>
         <button className={"bg-primary px-4 py-2 w-36 my-2 text-contras hover:bg-secondary"} onClick={buttonHandle}>Get in Touch</button>
